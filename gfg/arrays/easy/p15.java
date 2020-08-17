e9b532ce1816910fc74722b25089aaae84590e57
amurto <amurtobasu@gmail.com>
2020-08-17 20:24:26 +0530

    p16

--- gfg/arrays/easy/p15.java
+++ gfg/arrays/easy/p15.java
@@ -4,6 +4,7 @@
 import java.util.*;
 import java.io.*;
 
+// Approach using Queue
 public class p15 {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -14,28 +15,56 @@ public class p15 {
             Deque<Integer> d = new LinkedList<Integer>();
             String[] arr = br.readLine().split(" ");
 
-            for (int i=0; i<n; i++) 
+            for (int i = 0; i < n; i++)
                 a[i] = Integer.parseInt(arr[i]);
-            
+
             int k = Integer.parseInt(br.readLine());
-            for (int i=0; i<k; i++) 
-                if (a[i]<0)
+            for (int i = 0; i < k; i++)
+                if (a[i] < 0)
                     d.add(i);
-            
-            for (int i=k; i<n;i++) {
+
+            for (int i = k; i < n; i++) {
                 if (d.isEmpty())
-                    System.out.print(0+" ");
+                    System.out.print(0 + " ");
                 else {
-                    if (d.peek() == i-k)
-                        System.out.print(a[d.poll()]+" ");
+                    if (d.peek() == i - k)
+                        System.out.print(a[d.poll()] + " ");
                     else
-                        System.out.print(a[d.peek()]+" ");
+                        System.out.print(a[d.peek()] + " ");
                 }
 
                 if (a[i] < 0)
                     d.add(i);
             }
             System.out.print(d.isEmpty() ? 0 : a[d.poll()]);
+            System.out.println();
+        }
+    }
+}
+
+// Faster Approach
+public class p15 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(br.readLine());
+        while (t-- > 0) {
+            int n = Integer.parseInt(br.readLine());
+            int[] a = new int[n];
+            String[] arr = br.readLine().split(" ");
+            for (int i = 0; i < n; i++)
+                a[i] = Integer.parseInt(arr[i]);
+
+            int k = Integer.parseInt(br.readLine());
+            int cur = 0;
+            for (int i = k - 1; i < n; i++) {
+                if (cur < i - k + 1)
+                    cur++;
+                if (a[cur] >= 0) {
+                    while (cur < i && a[cur] >= 0)
+                        cur++;
+                }
+                System.out.print(a[cur] < 0 ? a[cur] + " " : 0 + " ");
+            }
             System.out.println();
         }
     }
---
+++ gfg/arrays/easy/p16.java
@@ -0,0 +1,11 @@
+import java.util.*;
+import java.io.*;
+
+public class p16 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(br.readLine());
+        while (t-- > 0) {
+        }
+    }
+}
