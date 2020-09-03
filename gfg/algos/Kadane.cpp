c496da939c794f43aa54dcc8a7e80880a9bc476d
Amurto Basu <49753722+amurto@users.noreply.github.com>
2020-09-03 15:30:09 +0530

    code

---
+++ gfg/algos/Kadane.cpp
@@ -0,0 +1,21 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int t;
+    cin>>t;
+    while (t-->0) {
+        int n;
+        cin>>n;
+        int a[n];
+        for (int i=0; i<n; i++) 
+            cin>>a[i];
+        int sum=0, maximum=INT_MIN;
+        for (int i=0; i<n; i++) {
+            sum = max(sum+a[i], a[i]);
+            maximum=max(sum, maximum);
+        }
+        cout<<maximum<<endl;
+    }
+    return 0;
+}
