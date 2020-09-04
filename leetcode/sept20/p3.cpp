d571a606b4d12263a1790846110aa994fb7a795f
Amurto Basu <49753722+amurto@users.noreply.github.com>
2020-09-04 18:16:29 +0530

    prob

---
+++ leetcode/sept20/p3.cpp
@@ -0,0 +1,42 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+bool repeatedSubstringPattern(string s)
+{
+    int n= s.length();
+    if (n==1)
+        return false;
+    char c[n];
+    for (int i=0;i < n; i++)
+        c[i] = s[i];
+    for (int i=1;i<=n/2; i++) {
+        if (n%i==0 && c[i]==c[0]) {
+            bool ans = true;
+            for (int j=0; j<i;j++) {
+                int idx=i+j;
+                while (idx<n && c[idx] == c[j])
+                    idx+=i;
+                if (idx<n) {
+                    ans=false;
+                    break;
+                }
+            }
+            if (ans)
+                return true;
+        }
+    }
+    return false;
+}
+
+int main()
+{
+    int t;
+    cin >> t;
+    while (t-- > 0)
+    {
+        string s;
+        cin>>s;
+        cout<<repeatedSubstringPattern(s);
+    }
+    return 0;
+}
