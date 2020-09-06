b953055fea5f4df7984eb98fcebe2a2ffba90061
amurto <amurtobasu@gmail.com>
2020-09-06 13:07:54 +0530

    code

--- gfg/strings/easy/p2.cpp
+++ gfg/strings/easy/p2.cpp
@@ -20,7 +20,6 @@ int main()
         for (int i = 0; i < n; i++)
             c[i] = s[i];
         string rev="", cur="";
-        int cur_num = 0, sum = 0, l = 0;
         for (int i = n - 1; i >= 0; i--)
         {
             if (c[i] == '.')
