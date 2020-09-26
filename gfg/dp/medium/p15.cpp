5f6297fc7565fe7d7bc0e9251c74d174239d1b49
Amurto Basu <49753722+amurto@users.noreply.github.com>
2020-09-26 16:54:44 +0530

    numeric keypad

--- gfg/dp/medium/p15.cpp
+++ gfg/dp/medium/p15.cpp
@@ -1,32 +1,41 @@
 // https://practice.geeksforgeeks.org/problems/largest-square-formed-in-a-matrix/0
-// Largest square formed in a matrix 
+// Largest square formed in a matrix
 
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
 
 typedef long long ll;
 #define pb push_back
 
-int main() {
+int DP[51][51];
+int LSQ(vector<vector<char>> &C, int i, int j)
+{
+    if (i < 0 || j < 0 || C[i][j] == '0')
+        return 0;
+    if (DP[i][j] < 0)
+        DP[i][j] = 1 + min(LSQ(C, i - 1, j - 1), min(LSQ(C, i - 1, j), LSQ(C, i, j - 1)));
+    return DP[i][j];
+}
+
+int main()
+{
     int t;
-    cin>>t;
-    while (t-->0) {
-        int n,m;
-        cin>>n>>m;
-        vector<vector<int>> C(n, vector<int>(m, 0));
-        for (int i=0; i<n; i++) 
-            for (int j=0; j<m; j++)
-                cin>>C[i][j];
-
-        for (int i=0; i<n; i++) {
-            for (int j=0; j<m; j++)
-                cout<<C[i][j]<<" ";
-            cout<<endl;
-        }
-
-        
-        cout<<endl;
-
+    cin >> t;
+    while (t-- > 0)
+    {
+        int n, m;
+        cin >> n >> m;
+        vector<vector<char>> C(n, vector<char>(m, 0));
+        for (int i = 0; i < n; i++)
+            for (int j = 0; j < m; j++)
+                cin >> C[i][j];
+        memset(DP, -1, sizeof(DP));
+        int MAX = 0;
+        for (int i = n - 1; i >= 0; i--)
+            for (int j = m - 1; j >= 0; j--)
+                if (C[i][j] == '1')
+                    MAX = max(MAX, LSQ(C, i, j));
+        cout << MAX << endl;
     }
     return 0;
 }
---
+++ gfg/dp/medium/p16.cpp
@@ -0,0 +1,54 @@
+// https://practice.geeksforgeeks.org/problems/mobile-numeric-keypad5456/1
+// Mobile numeric keypad
+
+// 1 2 3
+// 4 5 6
+// 7 8 9
+// * 0 #
+
+#include<bits/stdc++.h>
+using namespace std;
+
+typedef long long ll;
+#define pb push_back
+int di[5] = {0, 1, -1, 0, 0}, dj[5] = {0, 0, 0, -1, 1};
+ll DP[4][3][26];
+
+bool valid(int i, int j) {
+    if (i<0 || i>3 || j>2 || j<0 || (i==3 && j!=1))
+        return false;
+    return true;
+}
+
+ll MNK(int i, int j, int N) {
+    if (!valid(i,j))
+        return 0;
+    if (N==1)
+        return 1;
+    if (DP[i][j][N] < 0) {
+        DP[i][j][N] = 0;
+        for (int k=0; k<5; k++)
+            DP[i][j][N]+=MNK(i+di[k], j+dj[k], N-1);
+    }
+    return DP[i][j][N];
+}
+
+ll getCount(int N) {
+    ll ans = 0;
+    for (int i=0; i<4; i++)
+        for (int j=0; j<3; j++) 
+            ans+=MNK(i,j,N);
+    return ans;
+}
+
+int main() {
+    int t;
+    cin>>t;
+    memset(DP, -1, sizeof(DP));
+    while (t-->0) {
+        int N;
+        cin>>N;
+        cout<<getCount(N)<<endl;
+    }
+    return 0;
+}
