bbd61817639780da91f0026545a17a2de00a3b82
Amurto Basu <49753722+amurto@users.noreply.github.com>
2020-10-05 13:23:15 +0530

    c

--- codeforces/contests/round#675-div2/p3.cpp
+++ codeforces/contests/round#675-div2/p3.cpp
@@ -1,10 +1,10 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
 
 typedef long long ll;
 #define pb push_back
 
-ll MOD = 1e9+7;
+ll MOD = 1e9 + 7;
 
 ll add(ll x, ll y)
 {
@@ -21,27 +21,46 @@ ll mul(ll x, ll y)
     return (x * 1ll * y) % MOD;
 }
 
-int main() {
+int main()
+{
     ios_base::sync_with_stdio(false);
     cin.tie(0);
     cout.tie(0);
     string str;
-    cin>>str;
+    cin >> str;
     int n = str.length();
-    ll arr[n], ans=0;
-    ll DP[n][n];
-    for (int i=0; i<n; i++) {
+    ll arr[n], POW[n], SP[n], ans = 0, cur=0;
+
+    // Break the number into array
+    for (int i = 0; i < n; i++) 
         arr[i] = str[i] - 48;
-        DP[i][i] = arr[i];
-        ans = add(ans, arr[i]);
+
+    // Compute Powers of 10
+    // Sum of Powers is all 1s
+    // POW[3] = 1000
+    // SP[3] = 1111
+    POW[0] = 1;
+    SP[0] = 1;
+    for (int i = 1; i < n; i++) {
+        POW[i] = mul(POW[i - 1], 10);
+        SP[i] = add(POW[i], SP[i-1]);
     }
-    for (int gap=1; gap<n; gap++) {
-        for (int i=0, j=gap; j<n; j++) {
-            DP[i][j] = add(mul(DP[i][j-1],10), arr[j]);
-            cout<<DP[i][j]<<"\n";
-            ans = add(ans, DP[i][j]);
-        }
+
+    // Compute prices from the left
+    // Substring is removed from the right
+    for (int i = 0; i < n-1; i++) {
+        cur = add(mul(cur, 10), arr[i]);
+        ans = add(ans, mul(cur, SP[n-2-i]));
     }
-    cout<<ans<<"\n";
+
+    // Compute Prices from the right
+    // Substring is removed from left
+    cur = 0;
+    for (int i = n - 1; i > 0; i--) {
+        cur = add(mul(arr[i], POW[n-1-i]), cur);
+        ans = add(ans, mul(cur, i));
+    }
+
+    cout << ans << "\n";
     return 0;
 }
