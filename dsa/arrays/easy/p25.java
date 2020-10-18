89169fa808f643819ca195c6bc8eafb5f4e9ac3c
amurto <amurtobasu@gmail.com>
2020-10-18 14:46:39 +0530

    code

--- dsa/arrays/easy/p25.java
+++ dsa/arrays/easy/p25.java
@@ -4,7 +4,7 @@
 import java.util.*;
 import java.io.*;
 
-public class A {
+public class p25 {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int t = Integer.parseInt(br.readLine());
