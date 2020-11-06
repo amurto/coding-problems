e674aa3f66b38824510c18219d0886d1f150197b
Amurto Basu <49753722+amurto@users.noreply.github.com>
2020-11-06 16:59:07 +0530

    adhoc

--- codeforces/practice/1300/707B.cpp
+++ codeforces/practice/1300/707B.cpp
@@ -1,24 +1,47 @@
 // https://codeforces.com/problemset/problem/707/B
 // Bakery
 
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
 
 typedef long long ll;
 #define pb push_back
 
-int main() {
+int main()
+{
     ios_base::sync_with_stdio(false);
     cin.tie(0);
     cout.tie(0);
-    int t;
-    cin>>t;
-    while (t-->0) {
-        int n;
-        cin>>n;
-        int a[n];
-        for (int i=0; i<n; i++) 
-            cin>>a[i];
+    int n, m, k, u, v, w;
+    cin >> n >> m >> k;
+    vector<bool> storage(n + 1);
+    vector<vector<pair<int, int>>> g(n + 1);
+    while (m-- > 0)
+    {
+        cin >> u >> v >> w;
+        g[u].pb({v, w});
+        g[v].pb({u, w});
     }
+    while (k-- > 0)
+    {
+        cin >> u;
+        storage[u] = true;
+    }
+    int ans = -1;
+    for (int i = 1; i <= n; i++)
+    {
+        if (!storage[i])
+            continue;
+        for (pair<int, int> edge : g[i])
+        {
+            if (storage[edge.first])
+                continue;
+            if (ans == -1)
+                ans = edge.second;
+            else
+                ans = min(ans, edge.second);
+        }
+    }
+    cout << ans << "\n"; 
     return 0;
 }
