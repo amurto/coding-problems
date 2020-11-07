0b41e58231bc66fe7218f669292f0ad4b186bf82
Amurto Basu <49753722+amurto@users.noreply.github.com>
2020-11-07 16:38:54 +0530

    code

--- contests/hackerearth/october-easy-2020/p2.cpp
+++ contests/hackerearth/october-easy-2020/p2.cpp
@@ -4,18 +4,43 @@ using namespace std;
 typedef long long ll;
 #define pb push_back
 
+int beautyFactor(int num) {
+    while (num>=10) {
+        int temp = 0;
+        while (num>0) {
+            temp += num%10;
+            num/=10;
+        }
+        num = temp;
+    }
+    return num;
+}
+
+int bf(vector<bool> &vis, int k, int b, int num) {
+    if (k==0) {
+        if (beautyFactor(num) == b)
+            return num;
+        return -1;
+    }
+    for (int i=1; i<=9; i++) {
+        if (vis[i])
+            continue;
+        vis[i] = true;
+        int res = bf(vis, k-1, b, num*10 + i);
+        if (res!=-1)
+            return res;
+        vis[i] = false;
+    }
+    return -1;
+}
+
 int main() {
     ios_base::sync_with_stdio(false);
     cin.tie(0);
     cout.tie(0);
-    int t;
-    cin>>t;
-    while (t-->0) {
-        int n;
-        cin>>n;
-        int a[n];
-        for (int i=0; i<n; i++) 
-            cin>>a[i];
-    }
+    int b, k;
+    cin >> b >> k;
+    vector<bool> vis(10);
+    cout << bf(vis, k, b, 0) << "\n";
     return 0;
 }
