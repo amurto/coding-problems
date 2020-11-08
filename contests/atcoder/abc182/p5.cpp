a3567ec83ad37623f4603e1d0aec4c4e3a9f113d
Amurto Basu <49753722+amurto@users.noreply.github.com>
2020-11-08 20:38:23 +0530

    e

--- contests/atcoder/abc182/p5.cpp
+++ contests/atcoder/abc182/p5.cpp
@@ -2,40 +2,29 @@
 using namespace std;
 
 typedef long long ll;
-typedef pair<int, pair<int, int>> piii;
 #define pb push_back
 
 int di[4] = {0, 1, 0, -1}, dj[4] = {1, 0, -1, 0};
 
-bool valid(int &r, int &c, int &h, int &w)
+bool valid(int r, int c, int h, int w)
 {
     return (r > 0 && r <= h && c > 0 && c <= w);
 }
-
 int main()
 {
     ios_base::sync_with_stdio(false);
     cin.tie(0);
     cout.tie(0);
-    int h, w, n, m, a, b, c, d, r, c;
+    int h, w, n, m, a, b, c, d;
     cin >> h >> w;
     vector<vector<int>> grid(h + 1, vector<int>(w + 1));
     vector<vector<bool>> vis(h + 1, vector<bool>(w + 1));
     cin >> n >> m;
-    queue<piii> q;
-    int res = n;
     // 1 for bulb
     while (n-- > 0)
     {
         cin >> a >> b;
         grid[a][b] = 1;
-        for (int dir = 0; dir < 4; dir++)
-        {
-            r = r + di[dir];
-            c = c + dj[dir];
-            if (valid(r, c, h, w))
-                q.push({r, c});
-        }
     }
 
     // 2 for block
@@ -56,7 +45,7 @@ int main()
             for (int dir = 0; dir < 4; dir++)
             {
                 int r = i + di[dir], c = j + dj[dir];
-                while (valid(r, c, h, w) && grid[r][c] < 2)
+                while (valid(r, c, h, w) && grid[r][c] < 1)
                 {
                     if (!vis[r][c])
                         res++;
