bdec22f50f3126c47577fd36da81887489bca8f3
amurto <amurtobasu@gmail.com>
2020-11-09 13:50:29 +0530

    code

---
+++ contests/atcoder/abc182/p6.cpp
@@ -0,0 +1,20 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+typedef long long ll;
+#define pb push_back
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(0);
+    cout.tie(0);
+    int n;
+    ll x;
+    cin >> n >> x;
+    vector<ll> arr(n);
+    for (int i=0; i,n; i++)
+        cin >> arr[i];
+    
+
+    return 0;
+}
