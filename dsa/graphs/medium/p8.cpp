e50f40899a496d57f71f0536964d284f183af7fd
Amurto Basu <49753722+amurto@users.noreply.github.com>
2020-11-21 17:14:51 +0530

    code

--- dsa/graphs/medium/p8.cpp
+++ dsa/graphs/medium/p8.cpp
@@ -1,10 +1,11 @@
-// https://www.hackerearth.com/practice/algorithms/graphs/minimum-spanning-tree/practice-problems/algorithm/pilgrims-and-portals/
-// Pilgrims and Portals
+// https://www.hackerearth.com/practice/algorithms/graphs/minimum-spanning-tree/practice-problems/algorithm/grid-of-many-xors-de84b766/
+// Grid of Many Xors
 
 #include <bits/stdc++.h>
 using namespace std;
 
 typedef long long ll;
+typedef pair<int, pair<int, int>> piii;
 #define pb push_back
 
 int main()
@@ -16,16 +17,36 @@ int main()
     cin >> t;
     while (t-- > 0)
     {
-        int n, m, k, u, v, w;
-        cin >> n >> m >> k;
-        vector<vector<pair<int, int>>> g(n + 1);
-        for (int i = 0; i < m; i++)
+        int n, m, r1, c1, r2, c2, cost = 0;
+        int di[4] = {0, 1, 0, -1}, dj[4] = {1, 0, -1, 0};
+        cin >> n >> m >> r1 >> c1 >> r2 >> c2;
+        r1--;
+        c1--;
+        r2--;
+        c2--;
+        vector<vector<int>> grid(n, vector<int>(m));
+        vector<vector<bool>> vis(n, vector<bool>(m));
+        for (int i = 0; i < n; i++)
+            for (int j = 0; j < m; j++)
+                cin >> grid[i][j];
+        priority_queue<piii, vector<piii>, greater<piii>> pq;
+        pq.push({0, {r1, c1}});
+        while (!pq.empty())
         {
-            cin >> u >> v >> w;
-            g[u].pb({v, w});
-            g[v].pb({u, w});
+            piii from = pq.top();
+            pq.pop();
+            if (vis[from.second.first][from.second.second])
+                continue;
+            vis[from.second.first][from.second.second] = true;
+            cost += from.first;
+            for (int dir = 0; dir < 4; dir++)
+            {
+                int i = from.second.first + di[dir], j = from.second.second + dj[dir];
+                if (i >= 0 && i < n && j >= 0 && j < m && !vis[i][j])
+                    pq.push({(grid[from.second.first][from.second.second] ^ grid[i][j] + 1)/2, {i, j}});
+            }
         }
-        
+        cout << cost << "\n";
     }
     return 0;
 }
