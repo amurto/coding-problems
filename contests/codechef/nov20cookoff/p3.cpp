07cd553d48fc7182036effb7ab9a702d900c0cf9
Amurto Basu <49753722+amurto@users.noreply.github.com>
2020-11-24 16:12:32 +0530

    co

--- contests/codechef/nov20cookoff/p3.cpp
+++ contests/codechef/nov20cookoff/p3.cpp
@@ -4,60 +4,61 @@ using namespace std;
 typedef long long ll;
 #define pb push_back
 
-ll res = 100000000000;
-
-void hw(ll merge, vector<ll> arr)
+int hw(int merge, vector<int> arr)
 {
     if (merge == 0)
     {
-        ll sum = 0;
-        for (ll val : arr)
+        int sum = 0;
+        for (int val : arr)
             sum += val;
-        res = min(res, sum);
-        return;
+        return sum;
     }
+    int sum = INT_MAX;
     for (int i = 0; i < arr.size(); i++)
     {
         for (int j = i + 1; j < arr.size(); j++)
         {
-            vector<ll> farr;
+            vector<int> farr;
             farr.pb(arr[i] * arr[j]);
             for (int k = 0; k < arr.size(); k++)
                 if (k != i && k != j)
                     farr.pb(arr[k]);
-            hw(merge - 1, farr);
+            sum = min(sum, hw(merge - 1, farr));
         }
     }
+    return sum;
 }
 
-ll solve()
+int solve()
 {
-    ll k, x;
+    int k, x;
     cin >> k >> x;
 
-    // get factors
-    vector<ll> fact;
-    for (ll i = 2; i * i <= x; i++)
+    vector<int> fact;
+    for (int i = 2; i * i <= x; i++)
     {
-        while (x % i == 0)
+        if (x % i == 0)
         {
-            x /= i;
-            fact.pb(i);
+            int temp = 1;
+            while (x % i == 0)
+            {
+                x /= i;
+                temp *= i;
+            }
+            fact.pb(temp);
         }
     }
     if (x > 1)
         fact.pb(x);
-
     if (fact.size() <= k)
     {
-        ll sum = 0;
-        for (ll val : fact)
+        int sum = 0;
+        for (int val : fact)
             sum += val;
         sum += k - fact.size();
         return sum;
     }
-    hw(fact.size() - k, fact);
-    return res;
+    return hw(fact.size() - k, fact);
 }
 
 int main()
@@ -68,9 +69,6 @@ int main()
     int t;
     cin >> t;
     while (t-- > 0)
-    {
-        res = 100000000000;
         cout << solve() << "\n";
-    }
     return 0;
 }
