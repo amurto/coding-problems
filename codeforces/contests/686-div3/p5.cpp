e34e1fe355c44eebb69558ae6a0431525fd849b5
Amurto Basu <49753722+amurto@users.noreply.github.com>
2020-11-25 18:59:08 +0530

    e

--- codeforces/contests/686-div3/p5.cpp
+++ codeforces/contests/686-div3/p5.cpp
@@ -1,21 +1,54 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
 
 typedef long long ll;
 #define pb push_back
 
-int main() {
+ll solve()
+{
+    int n, u, v;
+    cin >> n;
+    vector<set<int>> g(n + 1);
+    vector<int> cnt(n + 1, 1);
+    for (int i = 0; i < n; i++)
+    {
+        cin >> u >> v;
+        g[u].insert(v);
+        g[v].insert(u);
+    }
+    queue<int> leaf;
+    for (int i = 1; i <= n; i++)
+        if (g[i].size() == 1)
+            leaf.push(i);
+
+    while (!leaf.empty())
+    {
+        int cur = leaf.front();
+        leaf.pop();
+        int parent = *g[cur].begin();
+        cnt[parent] += cnt[cur];
+        cnt[cur] = 0;
+        g[parent].erase(cur);
+        if (g[parent].size() == 1)
+            leaf.push(parent);
+    }
+
+    ll paths = 0;
+    for (int i = 1; i <= n; i++)
+    {
+        paths += cnt[i] * 1ll * (cnt[i] - 1) / 2;
+        paths += cnt[i] * 1ll * (n - cnt[i]);
+    }
+    return paths;
+}
+int main()
+{
     ios_base::sync_with_stdio(false);
     cin.tie(0);
     cout.tie(0);
     int t;
-    cin>>t;
-    while (t-->0) {
-        int n;
-        cin>>n;
-        vector<int> arr(n);
-        for (int i=0; i<n; i++) 
-            cin>>arr[i];
-    }
+    cin >> t;
+    while (t-- > 0)
+        cout << solve() << "\n";
     return 0;
 }
