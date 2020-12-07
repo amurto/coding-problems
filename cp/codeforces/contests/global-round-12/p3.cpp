64c6eec5603631ae3c53f7180e5143a02d7babd0
Amurto Basu <49753722+amurto@users.noreply.github.com>
2020-12-07 13:05:13 +0530

    practice

--- cp/codeforces/contests/global-round-12/p3.cpp
+++ cp/codeforces/contests/global-round-12/p3.cpp
@@ -4,7 +4,6 @@ using namespace std;
 typedef long long ll;
 #define pb push_back
 
-int di[4] = {0, 1, 0, -1}, dj[4] = {1, 0, -1, 0};
 int main()
 {
     ios_base::sync_with_stdio(false);
@@ -17,41 +16,38 @@ int main()
         int n, cnt = 0, m = 0;
         cin >> n;
         vector<vector<char>> grid(n, vector<char>(n));
+        vector<pair<int, int>> X = {{0, 0}, {0, 1}, {0, 2}};
         for (int i = 0; i < n; i++)
         {
             for (int j = 0; j < n; j++)
             {
                 cin >> grid[i][j];
                 if (grid[i][j] == 'X')
+                {
                     cnt++;
+                    X[(i + j) % 3].first++;
+                }
             }
         }
+        sort(X.begin(), X.end());
+        int mn = X[0].second;
         for (int i = 0; i < n; i++)
         {
             for (int j = 0; j < n; j++)
             {
-                bool d = true;
-                if (i - 1 >= 0 && i - 1 < n && j - 1 >= 0 && j - 1 < n && grid[i - 1][j - 1] != '.')
-                    d = false;
-                if (i + 1 >= 0 && i + 1 < n && j + 1 >= 0 && j + 1 < n && grid[i + 1][j + 1] == '.')
-                    d = false;
-                if (grid[i][j] == '.')
-                    d = false;
-                if (d) {
-                    m++;
+                if ((i + j) % 3 == mn && grid[i][j] == 'X')
+                {
                     grid[i][j] = 'O';
+                    m++;
                 }
             }
         }
         for (int i = 0; i < n; i++)
         {
             for (int j = 0; j < n; j++)
-            {
                 cout << grid[i][j];
-            }
             cout << "\n";
         }
-        cout << cnt << " " << m << "\n";
     }
     return 0;
 }
--- cp/codeforces/practice/1600/560C.cpp
+++ cp/codeforces/practice/1600/560C.cpp
@@ -1,24 +1,32 @@
 // https://codeforces.com/contest/560/problem/C
 // Gerald's Hexagon
 
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
 
 typedef long long ll;
 #define pb push_back
 
-int main() {
+int sap(int a, int n)
+{
+    return n * (a + n - 1);
+}
+
+int main()
+{
     ios_base::sync_with_stdio(false);
     cin.tie(0);
     cout.tie(0);
-    int t;
-    cin>>t;
-    while (t-->0) {
-        int n;
-        cin>>n;
-        vector<int> arr(n);
-        for (int i=0; i<n; i++) 
-            cin>>arr[i];
-    }
+    vector<int> s(6);
+    cin >> s[0] >> s[1] >> s[2] >> s[3] >> s[4] >> s[5];
+    // upper trapezium with ap
+    int t1 = sap(s[0] * 2 + 1, min(s[1], s[5]));
+
+    // lower trapezium with ap
+    int t2 = sap(s[3] * 2 + 1, min(s[2], s[4]));
+
+    // middle rhombus
+    int t3 = (max(s[1], s[5]) - min(s[1], s[5])) * (s[0] + min(s[1], s[5])) * 2;
+    cout << t1 + t2 + t3 << "\n";
     return 0;
 }
