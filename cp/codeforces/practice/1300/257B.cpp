b9123d2f8cd6bce0a85470d91bf8aec7aa066971
amurto <amurtobasu@gmail.com>
2020-12-08 23:31:01 +0530

    Update 257B.cpp

--- cp/codeforces/practice/1300/257B.cpp
+++ cp/codeforces/practice/1300/257B.cpp
@@ -7,21 +7,12 @@ using namespace std;
 typedef long long ll;
 #define pb push_back
 
-void solve()
+int solve(vector<int> cube, int f)
 {
-    int n, m, chance = 1;
-    vector<int> cube(2), col;
-    cin >> cube[0] >> cube[1];
-    if (cube[0] <= cube[1])
-    {
-        col.pb(0);
-        cube[0]--;
-    }
-    else
-    {
-        col.pb(1);
-        cube[1]--;
-    }
+    int chance = 1;
+    vector<int> col;
+    col.pb(f);
+    cube[f]--;
     while (cube[0] > 0 && cube[1] > 0)
     {
         int last = col.back();
@@ -35,18 +26,11 @@ void solve()
         col.pb(0);
     while (cube[1]-- > 0)
         col.pb(1);
-    vector<int> sc(2);
-    cout << col[0];
+    int sc = 0;
     for (int i = 1; i < col.size(); i++)
-    {
-        cout << col[i];
         if (col[i] == col[i - 1])
-            sc[0]++;
-        else
-            sc[1]++;
-    }
-    cout << "\n";
-    cout << sc[0] << " " << sc[1] << "\n";
+            sc++;
+    return sc;
 }
 
 int main()
@@ -54,6 +38,9 @@ int main()
     ios_base::sync_with_stdio(false);
     cin.tie(0);
     cout.tie(0);
-    solve();
+    vector<int> cube(2);
+    cin >> cube[0] >> cube[1];
+    int p = max(solve(cube, 0), solve(cube, 1));
+    cout << p << " " << cube[0] + cube[1] - p - 1 << "\n";
     return 0;
 }
