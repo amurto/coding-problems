ef3ff3df09caeaa9a3f632218988d5ee46cc65ea
amurto <amurtobasu@gmail.com>
2020-12-28 15:47:16 +0530

    maths

--- cp/hackerrank/practice/p2.cpp
+++ cp/hackerrank/practice/p2.cpp
@@ -6,11 +6,12 @@ using namespace std;
 
 typedef long long ll;
 #define pb push_back
-const ll MOD = 1e9 + 7;
 
-ll power(ll n, ll m, ll p)
+const int MOD = 1e9 + 7;
+
+int power(int n, int m, int p)
 {
-    ll res = 1;
+    int res = 1;
     while (m > 0)
     {
         if (m & 1)
@@ -18,16 +19,25 @@ ll power(ll n, ll m, ll p)
         n = (n * 1ll * n) % p;
         m /= 2;
     }
-    return res % p;
+    return res;
 }
 
+// We have to find 3^k where k = 3^n
+// a^m-1 mod m = 1 where m is prime (Fermat's little Theorem) .... (1)
+// k can be represented as q * (m - 1) + r
+// ans = 3^(q * (m - 1) + r) mod m
+// 3^(m-1 * q) mod m * 3^r mod m
+// By (1), 3^(m-1) mod m = 1 
+// Substituting, ans = 1^q * 3^r mod m
+// ans = 3^r mod m where r = 3^n mod m-1
+// Now fast exponentiation is possible
 int main()
 {
     ios_base::sync_with_stdio(false);
     cin.tie(0);
     cout.tie(0);
-    ll n;
+    int n;
     cin >> n;
-    cout << power(3, power(3, n, MOD), MOD) << "\n";
+    cout << power(3, power(3, n, MOD - 1), MOD) << "\n";
     return 0;
 }
