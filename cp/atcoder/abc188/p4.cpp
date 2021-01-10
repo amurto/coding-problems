0e364db09699a963575c62a52760e7ebe60faaf4
amurto <amurtobasu@gmail.com>
2021-01-10 20:14:44 +0530

    upsolve

--- cp/atcoder/abc188/p4.cpp
+++ cp/atcoder/abc188/p4.cpp
@@ -10,8 +10,24 @@ int main()
     cin.tie(0);
     cout.tie(0);
     int n;
-    ll cost, c;
-    cin >> n;
-    
+    ll a, b, c, cost;
+    cin >> n >> cost;
+    vector<pair<ll, ll>> seq;
+    for (int i = 0; i < n; i++)
+    {
+        cin >> a >> b >> c;
+        seq.pb({a, c});
+        seq.pb({b + 1, -c});
+    }
+    sort(seq.begin(), seq.end());
+    ll res = 0, cur = 0, last = 0;
+    for (int i = 0; i < seq.size(); i++)
+    {
+        res += (seq[i].first - last) * min(cost, cur);
+        cur += seq[i].second;
+        if (seq[i].first > last)
+            last = seq[i].first;
+    }
+    cout << res << "\n";
     return 0;
 }
