494c934c60beb40fba990ce3136592a36ff51210
amurto <amurtobasu@gmail.com>
2021-03-18 13:06:10 +0530

    practice

--- cp/codeforces/contests/708-div2/p5.cpp
+++ cp/codeforces/contests/708-div2/p5.cpp
@@ -4,7 +4,7 @@ using namespace std;
 typedef long long ll;
 #define pb push_back
 
-const int N = 1e7 + 5;
+const int N = 1e7 + 5, inf = 1e9 + 5;
 
 // O(n)
 vector<int> lp(N + 1), pr;
@@ -41,35 +41,35 @@ int fac(int x)
 
 int solve()
 {
-    int n, k, res = 0;
+    int n, k;
     cin >> n >> k;
-    vector<int> arr(n), c(n), nxt(n);
+    vector<int> arr(n + 1), prev(n + 1);
     map<int, vector<int>> mp;
-    for (int i = 0; i < n; i++)
+    for (int i = 1; i <= n; i++)
     {
         cin >> arr[i];
-        c[i] = fac(arr[i]);
-        mp[c[i]].pb(i);
+        arr[i] = fac(arr[i]);
+        mp[arr[i]].pb(i);
     }
     for (auto m : mp)
+        for (int i = m.second.size() - 1; i > 0; i--)
+            prev[m.second[i]] = m.second[i - 1];
+    vector<vector<int>> last(n + 1, vector<int>(k + 1)), dp(n + 1, vector<int>(k + 1, inf));
+    set<int> st;
+    for (int i = 1; i <= n; i++)
     {
-        for (int i = 0; i < m.second.size() - 1; i++)
-            nxt[m.second[i]] = m.second[i + 1];
-        nxt[m.second[m.second.size() - 1]] = n;
+        st.insert(prev[i]);
+        auto it = --st.end();
+        for (int j = 0; *it > 0 && j <= k; j++, it--)
+            last[i][j] = *it;
     }
-    int b = n;
-    for (int i = 0; i < n; i++)
-    {
-        if (i == b)
-        {
-            res++;
-            b = nxt[i];
-        }
-        else
-            b = min(b, nxt[i]);
-    }
-    res++;
-    return res;
+    for (int j = 0; j <= k; j++)
+        dp[0][j] = 0;
+    for (int i = 1; i <= n; i++)
+        for (int j = 0; j <= k; j++)
+            for (int x = 0; x <= k - j; x++)
+                dp[i][x + j] = min(dp[i][x + j], 1 + dp[last[i][j]][x]);
+    return dp[n][k];
 }
 
 int main()
