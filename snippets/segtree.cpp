7578150619ba0f1edc09eacf2acf7bc80acf74fb
Amurto Basu <49753722+amurto@users.noreply.github.com>
2021-04-06 19:50:03 +0530

    practice

--- snippets/segtree.cpp
+++ snippets/segtree.cpp
@@ -92,7 +92,7 @@ struct segtree
     {
         if (l > tr || r < tl)
             return identity_element;
-        if (tl >= l && tr <= r)
+        if (l <= tl && tr <= r)
             return t[v];
         pushdown(v, tl, tr);
         int32_t tm = (tl + tr) >> 1;
@@ -106,7 +106,7 @@ struct segtree
     {
         if (l > tr || r < tl)
             return;
-        if (tl >= l && tr <= r)
+        if (l <= tl && tr <= r)
         {
             apply(v, tl, tr, upd);
             return;
@@ -217,4 +217,54 @@ public:
     {
         return query(1, 0, len - 1, l, r, e);
     }
+};
+
+// X-------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------X
+
+// Range AP Updates
+struct node
+{
+    int v = 0; // identity
+    node() {}
+    node(int val)
+    {
+        v = val;
+    }
+    void merge(const node &l, const node &r)
+    {
+        v = l.v + r.v;
+    }
+};
+
+struct update
+{
+    int a = 0, d = 0, l = 0;
+    update() {}
+    update(int first_term, int diff, int first_idx)
+    {
+        a = first_term;
+        d = diff;
+        l = first_idx;
+    }
+
+    void fix(const int32_t &tl, const int32_t &tr)
+    {
+        a += (tl - l) * d;
+        l = tl;
+    }
+    // combine the current update with the other update
+    void combine(update &other, const int32_t &tl, const int32_t &tr)
+    {
+        a += other.a;
+        d += other.d;
+        l = tl;
+    }
+    // store the correct information in the node x
+    void apply(node &x, const int32_t &tl, const int32_t &tr)
+    {
+        x.v += ((tr - tl + 1) * (2 * a + (tr - tl) * d)) / 2;
+    }
+
+    // put at first line in apply function of segtree
+    // upd.fix(tl, tr);
 };
