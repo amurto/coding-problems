908dcee7144d1e85f8bd17b47a6371b70b0ea09c
Amurto Basu <49753722+amurto@users.noreply.github.com>
2021-04-07 20:12:08 +0530

    practice

---
+++ 819B.cpp
@@ -0,0 +1,209 @@
+// https://codeforces.com/contest/819/problem/B
+// Mister B and PR Shifts
+
+#include <bits/stdc++.h>
+using namespace std;
+
+typedef long long ll;
+#define pb push_back
+
+// Range AP Updates
+struct node
+{
+    ll v = 0; // identity
+    node() {}
+    node(ll val)
+    {
+        v = val;
+    }
+    void merge(const node &l, const node &r)
+    {
+        v = l.v + r.v;
+    }
+};
+
+struct update
+{
+    ll a = 0, d = 0, l = 0;
+    update() {}
+    update(ll first_term, ll diff, ll first_idx)
+    {
+        a = first_term;
+        d = diff;
+        l = first_idx;
+    }
+
+    void fix(const int32_t &tl, const int32_t &tr)
+    {
+        a += (tl - l) * d;
+        l = tl;
+    }
+    // combine the current update with the other update
+    void combine(update &other, const int32_t &tl, const int32_t &tr)
+    {
+        a += other.a;
+        d += other.d;
+        l = tl;
+    }
+    // store the correct information in the node x
+    void apply(node &x, const int32_t &tl, const int32_t &tr)
+    {
+        x.v += ((tr - tl + 1) * (2 * a + (tr - tl) * d)) / 2;
+    }
+
+    // put at first line in apply function of segtree
+    // upd.fix(tl, tr);
+};
+
+template <typename node, typename update>
+struct segtree
+{
+    int len;
+    vector<node> t;
+    vector<update> u;
+    vector<bool> lazy;
+    node identity_element;
+    update identity_transformation;
+    segtree(int l)
+    {
+        len = l;
+        t.resize(4 * len);
+        u.resize(4 * len);
+        lazy.resize(4 * len);
+        identity_element = node();
+        identity_transformation = update();
+    }
+
+    void pushdown(const int32_t &v, const int32_t &tl, const int32_t &tr)
+    {
+        if (!lazy[v])
+            return;
+        int32_t tm = (tl + tr) >> 1;
+        apply(v << 1, tl, tm, u[v]);
+        apply(v << 1 | 1, tm + 1, tr, u[v]);
+        u[v] = identity_transformation;
+        lazy[v] = 0;
+    }
+
+    void apply(const int32_t &v, const int32_t &tl, const int32_t &tr, update upd)
+    {
+        upd.fix(tl, tr);
+        if (tl != tr)
+        {
+            lazy[v] = 1;
+            u[v].combine(upd, tl, tr);
+        }
+        upd.apply(t[v], tl, tr);
+    }
+
+    template <typename T>
+    void build(const T &arr, const int32_t &v, const int32_t &tl, const int32_t &tr)
+    {
+        if (tl == tr)
+        {
+            t[v] = arr[tl];
+            return;
+        }
+        int32_t tm = (tl + tr) >> 1;
+        build(arr, v << 1, tl, tm);
+        build(arr, v << 1 | 1, tm + 1, tr);
+        t[v].merge(t[v << 1], t[v << 1 | 1]);
+    }
+
+    node query(const int32_t &v, const int32_t &tl, const int32_t &tr, const int32_t &l, const int32_t &r)
+    {
+        if (l > tr || r < tl)
+            return identity_element;
+        if (l <= tl && tr <= r)
+            return t[v];
+        pushdown(v, tl, tr);
+        int32_t tm = (tl + tr) >> 1;
+        node a = query(v << 1, tl, tm, l, r), b = query(v << 1 | 1, tm + 1, tr, l, r), ans;
+        ans.merge(a, b);
+        return ans;
+    }
+
+    // rupd = range update
+    void rupd(const int32_t &v, const int32_t &tl, const int32_t &tr, const int32_t &l, const int32_t &r, const update &upd)
+    {
+        if (l > tr || r < tl)
+            return;
+        if (l <= tl && tr <= r)
+        {
+            apply(v, tl, tr, upd);
+            return;
+        }
+        pushdown(v, tl, tr);
+        int32_t tm = (tl + tr) >> 1;
+        rupd(v << 1, tl, tm, l, r, upd);
+        rupd(v << 1 | 1, tm + 1, tr, l, r, upd);
+        t[v].merge(t[v << 1], t[v << 1 | 1]);
+    }
+
+public:
+    template <typename T>
+    void build(const T &arr)
+    {
+        build(arr, 1, 0, len - 1);
+    }
+    node query(const int32_t &l, const int32_t &r)
+    {
+        return query(1, 0, len - 1, l, r);
+    }
+    void rupd(const int32_t &l, const int32_t &r, const update &upd)
+    {
+        rupd(1, 0, len - 1, l, r, upd);
+    }
+};
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(0);
+    cout.tie(0);
+    int n;
+    cin >> n;
+    vector<int> arr(n);
+    for (int i = 0; i < n; i++)
+    {
+        cin >> arr[i];
+        arr[i]--;
+    }
+    segtree<node, update> s(n);
+    for (int i = 0; i < n; i++)
+    {
+        if (arr[i] <= i)
+        {
+            s.rupd(0, n - i - 1, update(i - arr[i], 1, 0));
+            int r = n - i;
+            if (r < n)
+            {
+                s.rupd(r, r + arr[i] - 1, update(arr[i], -1, r));
+                r += arr[i];
+            }
+            if (r < n)
+                s.rupd(r, n - 1, update(0, 1, r));
+        }
+        else
+        {
+            s.rupd(0, arr[i] - i, update(arr[i] - i, -1, 0));
+            int r = arr[i] - i + 1;
+            if (arr[i] < n - 1)
+            {
+                s.rupd(r, r + n - arr[i] - 2, update(1, 1, r));
+                r += n - arr[i] - 1;
+            }
+            if (r < n)
+                s.rupd(r, n - 1, update(arr[i], -1, r));
+        }
+    }
+    int mn = 0;
+    vector<int> d(n);
+    for (int i = 0; i < n; i++)
+        d[i] = s.query(i, i).v;
+    for (int i = 0; i < n; i++)
+        if (d[i] < d[mn])
+            mn = i;
+    cout << d[mn] << " " << mn << "\n";
+    return 0;
+}
