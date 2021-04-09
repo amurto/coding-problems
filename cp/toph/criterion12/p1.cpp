d703d55026515d6c4ea1d695eab8bb2540c0b706
Amurto Basu <49753722+amurto@users.noreply.github.com>
2021-04-09 20:13:10 +0530

    practice

---
+++ cp/toph/criterion12/p1.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+typedef long long ll;
+#define pb push_back
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(0);
+    cout.tie(0);
+    ll n;
+    cin >> n;
+    cout << n + 1 << "\n";
+    return 0;
+}
