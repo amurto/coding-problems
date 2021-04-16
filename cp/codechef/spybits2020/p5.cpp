af365d1b5bcabe4d00dd0be631bde9c3182cb65f
Amurto Basu <49753722+amurto@users.noreply.github.com>
2021-04-16 19:20:06 +0530

    practice

--- cp/codechef/spybits2020/p5.cpp
+++ cp/codechef/spybits2020/p5.cpp
@@ -2,46 +2,28 @@
 using namespace std;
 
 typedef long long ll;
+typedef pair<ll, ll> pii;
 #define pb push_back
 
-int moves(vector<ll> &pre, vector<ll> &suf, int n, ll l, ll r, ll target)
+pii get_range(vector<ll> &pre, vector<ll> &suf, ll l, ll r, int n, int m)
 {
-    int low = 0, high = n - 1, res = n - 1, diff = abs(target - pre[n - 1]);
-    if (pre[n - 1] == target)
-        return 0;
-    if (pre[n - 1] > target)
+    ll sum = pre[n - 1];
+    if (m == 0)
+        return {sum, sum};
+    return {sum + l * m - suf[m - 1], sum + r * m - pre[m - 1]};
+}
+
+bool merge_range(vector<vector<ll>> &pre, vector<vector<ll>> &suf, ll l, ll r, int n, int k, int m)
+{
+    pii cur = {1, 1e18};
+    for (int i = 0; i < k; i++)
     {
-        // decrease sum
-        while (low <= high)
-        {
-            int mid = low + (high - low) / 2;
-            int mn = l * (mid + 1);
-            if (suf[mid] - mn >= diff)
-            {
-                res = min(res, mid);
-                high = mid - 1;
-            }
-            else
-                low = mid + 1;
-        }
+        pii rg = get_range(pre[i], suf[i], l, r, n, m);
+        if (rg.first > cur.second || rg.second < cur.first)
+            return false;
+        cur = {max(cur.first, rg.first), min(cur.second, rg.second)};
     }
-    else
-    {
-        // increase sum
-        while (low <= high)
-        {
-            int mid = low + (high - low) / 2;
-            ll mx = r * (mid + 1);
-            if (mx - pre[mid] >= diff)
-            {
-                res = min(res, mid);
-                high = mid - 1;
-            }
-            else
-                low = mid + 1;
-        }
-    }
-    return res + 1;
+    return true;
 }
 
 int solve()
@@ -63,22 +45,18 @@ int solve()
         for (int j = 1; j < n; j++)
             suf[i][j] = suf[i][j - 1] + arr[i][j];
     }
-    ll low = l * n, high = r * n;
-    int res = n * k;
+    int low = 0, high = n, res = n;
     while (low <= high)
     {
-        ll mid1 = low + (high - low) / 3;
-        ll mid2 = high - (high - low) / 3;
-        int m1 = 0, m2 = 0;
-        for (int i = 0; i < k; i++)
-            m1 = max(m1, moves(pre[i], suf[i], n, l, r, mid1));
-        for (int i = 0; i < k; i++)
-            m2 = max(m2, moves(pre[i], suf[i], n, l, r, mid2));
-        res = min(res, min(m1, m2));
-        if (m1 >= m2)
-            low = mid1 + 1;
+        int mid = low + (high - low) / 2;
+        bool f = merge_range(pre, suf, l, r, n, k, mid);
+        if (f)
+        {
+            res = min(res, mid);
+            high = mid - 1;
+        }
         else
-            high = mid2 - 1;
+            low = mid + 1;
     }
     return res;
 }
