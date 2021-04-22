1da727d43f9b2a0c990826cdaae0359c0b7f7c08
amurto <amurtobasu@gmail.com>
2021-04-22 15:00:49 +0530

    practice

--- cp/google/kickstart2021B/p2.cpp
+++ cp/google/kickstart2021B/p2.cpp
@@ -4,78 +4,61 @@ using namespace std;
 typedef long long ll;
 #define pb push_back
 
-const ll inf = 1e18 + 7;
 int solve()
 {
-    int n, res = 2;
+    int n;
     cin >> n;
-    vector<ll> arr(n), pre(n, inf), suf(n, inf);
+    vector<int> arr(n);
     for (int i = 0; i < n; i++)
         cin >> arr[i];
-    if (n == 2)
-        return 2;
+    if (n <= 3)
+        return n;
+    int res = 3;
+    vector<int> left(n), right(n), dleft(n), dright(n);
     for (int i = 1; i < n; i++)
-        pre[i] = arr[i] - arr[i - 1];
+        dleft[i] = arr[i] - arr[i - 1];
     for (int i = n - 2; i >= 0; i--)
-        suf[i] = arr[i + 1] - arr[i];
-    vector<vector<int>> dp(2, vector<int>(n));
-    for (int j = 0; j < 2; j++)
-    {
-        ll d = arr[1] - arr[0];
-        dp[j][0] = 1;
-        dp[j][1] = 2;
-        for (int i = 2; i < n; i++)
-        {
-            if (arr[i] - arr[i - 1] == d)
-                dp[j][i] += 1 + dp[j][i - 1];
-            else
-            {
-                d = arr[i] - arr[i - 1];
-                dp[j][i] = 2;
-            }
-        }
-        reverse(arr.begin(), arr.end());
-    }
-    reverse(dp[1].begin(), dp[1].end());
-    // cout << "\n";
-    // for (int i = 0; i < n; i++)
-    //     cout << dp[0][i] << " ";
-    // cout << "\n";
-    // for (int i = 0; i < n; i++)
-    //     cout << dp[1][i] << " ";
-    // cout << "\n";
-    // for (int i = 0; i < n; i++)
-    //     cout << pre[i] << " ";
-    // cout << "\n";
-    // for (int i = 0; i < n; i++)
-    //     cout << suf[i] << " ";
-    // cout << "\n";
+        dright[i] = arr[i + 1] - arr[i];
+    left[0] = 1;
+    left[1] = 2;
+    for (int i = 2; i < n; i++)
+        left[i] = (dleft[i - 1] == dleft[i]) ? left[i - 1] + 1 : 2;
+    right[n - 1] = 1;
+    right[n - 2] = 2;
+    for (int i = n - 3; i >= 0; i--)
+        right[i] = (dright[i] == dright[i + 1]) ? right[i + 1] + 1 : 2;
     for (int i = 0; i < n; i++)
     {
-        res = max(res, max(dp[0][i], dp[1][i]));
-        if (i < n - 1)
-            res = max(res, 1 + dp[1][i + 1]);
+        res = max(res, max(left[i], right[i]));
         if (i > 0)
-            res = max(res, 1 + dp[0][i - 1]);
+            res = max(res, left[i - 1] + 1);
+        if (i < n - 1)
+            res = max(res, right[i + 1] + 1);
         if (i > 0 && i < n - 1)
         {
-            if (pre[i - 1] == inf && suf[i + 1] == inf)
+            int d = arr[i + 1] - arr[i - 1];
+            int p = d % 2, h = d / 2;
+            if (p == 0)
             {
-                if ((arr[i + 1] - arr[i - 1]) % 2 == 0)
-                    res = max(res, 3);
-            }
-            else
-            {
-                ll d1 = pre[i - 1], d2 = suf[i + 1], diff = arr[i + 1] - arr[i - 1];
-                ll p = diff % 2, h = diff / 2;
-                if (p == 0)
-                    res = max(res, 3);
-                if (d1 != inf && p == 0 && h == d1)
-                    res = max(res, dp[0][i - 1] + 2);
-                if (d2 != inf && p == 0 && h == d1)
-                    res = max(res, dp[1][i + 1] + 2);
-                if (d1 == d2 && p == 0 && h == d1)
-                    res = max(res, dp[0][i - 1] + 1 + dp[1][i + 1]);
+                if (i - 1 == 0)
+                {
+                    if (h == dright[i + 1])
+                        res = max(res, 2 + right[i + 1]);
+                }
+                else if (i + 1 == n - 1)
+                {
+                    if (h == dleft[i - 1])
+                        res = max(res, 2 + left[i - 1]);
+                }
+                else
+                {
+                    if (h == dright[i + 1])
+                        res = max(res, 2 + right[i + 1]);
+                    if (h == dleft[i - 1])
+                        res = max(res, 2 + left[i - 1]);
+                    if (dleft[i - 1] == dright[i + 1] && h == dleft[i - 1])
+                        res = max(res, left[i - 1] + 1 + right[i + 1]);
+                }
             }
         }
     }
