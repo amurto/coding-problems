f14649866654815671d8f6e93755c7127afb1a38
Amurto Basu <49753722+amurto@users.noreply.github.com>
2021-05-08 23:11:31 +0530

    practice

--- cp/atcoder/abc200/p5.cpp
+++ cp/atcoder/abc200/p5.cpp
@@ -5,6 +5,45 @@ typedef long long ll;
 #define pb push_back
 
 const int N = 3e6 + 5;
+void dfs(vector<vector<ll>> &dp, int n, ll k)
+{
+    int low = 1, high = 3 * n, sum = 3 * n;
+    while (low <= high)
+    {
+        int mid = low + (high - low) / 2;
+        if (dp[2][mid] >= k)
+        {
+            sum = min(sum, mid);
+            high = mid - 1;
+        }
+        else
+            low = mid + 1;
+    }
+    k -= dp[2][sum - 1];
+    vector<int> tmp(3);
+    for (int j = 0, u = 1; j < 2; j++, u--)
+    {
+        ll pre = 0, rem = 0;
+        int last = 1;
+        int mn = max(1, sum - u * n), mx = min(n, sum - u * 1);
+        for (int i = mn; pre < k && i <= mx; i++)
+        {
+            rem += pre;
+            pre += dp[u][sum - i];
+            last = i;
+        }
+        k -= rem;
+        sum -= last;
+        tmp[j] = last;
+        // cout << last << " " << sum << " " << k << "\n";
+        // cout << k << "\n";
+    }
+    tmp[2] = sum;
+    for (int t : tmp)
+        cout << t << " ";
+    cout << "\n";
+}
+
 void solve()
 {
     int n, mx;
@@ -28,35 +67,14 @@ void solve()
                 mx = i;
         }
     }
-    for (int j = 0; j < 3; j++)
-        for (int i = 1; i <= N; i++)
-            dp[j][i] += dp[j][i - 1];
-    vector<int> tmp;
-    for (int j = 2; j >= 0; j--)
+    for (int i = 1; i <= N; i++)
+        dp[2][i] += dp[2][i - 1];
+    // dfs(dp, n, k);
+    for (ll i = 1; i <= 1ll * n * n * n; i++)
     {
-        int low = 1, high = N, res = N, e = n;
-        while (low <= high)
-        {
-            int mid = low + (high - low) / 2;
-            if (dp[j][mid] >= k)
-            {
-                res = min(res, mid);
-                high = mid - 1;
-            }
-            else
-                low = mid + 1;
-        }
-        low = 1, high = n;
-        while (low <= high)
-        {
-            int mid = low + (high - low) / 2;
-        }
-        tmp.pb(res);
-        k -= dp[j][res - 1];
+        cout << i << " ";
+        dfs(dp, n, i);
     }
-
-    for (int i = 0; i < 3; i++)
-        cout << tmp[i] << " ";
 }
 
 int main()
