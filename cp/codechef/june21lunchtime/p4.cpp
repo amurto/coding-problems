db54577194e207268b2a990614ddb917149a9a64
Amurto Basu <49753722+amurto@users.noreply.github.com>
2021-06-28 20:23:38 +0530

    practice

--- cp/codechef/june21lunchtime/p4.cpp
+++ cp/codechef/june21lunchtime/p4.cpp
@@ -5,168 +5,201 @@ typedef long long ll;
 typedef pair<int, int> pii;
 #define pb push_back
 
-// for 64 bit, use mt19937_64
-mt19937 rng((unsigned int)chrono::steady_clock::now().time_since_epoch().count());
-// Just call rand(minValue, maxValue);
-// Note: it doesn't guarantee distinct values.
-int rand(int l, int r)
+class grid
 {
-    static mt19937
-        rng(chrono::steady_clock::now().time_since_epoch().count());
-    uniform_int_distribution<int> ludo(l, r);
-    return ludo(rng);
-}
-
-const int N = 50;
-int grid[N][N];
-
-int ti = rand(1, N - 2), tj = rand(1, N - 2), q = 0, correct = 0;
-
-int di[2] = {0, 1}, dj[2] = {1, 0};
-bool bfs(pii src, pii des)
-{
-    vector<vector<bool>> vis(N, vector<bool>(N));
-    queue<pii> q;
-    vis[src.first][src.second] = true;
-    q.push(src);
-    while (!q.empty())
+public:
+    int n;
+    vector<string> mat;
+    grid() {}
+    grid(int n) : n(n)
     {
-        pii cur = q.front();
-        q.pop();
-        for (int dir = 0; dir < 2; dir++)
+        mat.resize(n);
+        for (int i = 0; i < n; i++)
         {
-            int i = cur.first + di[dir], j = cur.second + dj[dir];
-            if (i == des.first && j == des.second)
-                return true;
-            if (i < N && j < N && !vis[i][j] && grid[i][j] == 1)
-            {
-                vis[i][j] = true;
-                q.push({i, j});
-            }
+            string tmp = "";
+            for (int j = 0; j < n; j++)
+                tmp.pb('0');
+            mat[i] = tmp;
         }
     }
-    return false;
-}
 
-void colorRow(int l, int r)
-{
-    for (int i = l; i <= r; i++)
-        for (int j = 0; j < N; j++)
-            grid[i][j] = 1;
-}
-
-void colorCol(int l, int r)
-{
-    for (int i = 0; i < N; i++)
-        for (int j = l; j <= r; j++)
-            grid[i][j] = 1;
-}
-
-void clearGrid()
-{
-    memset(grid, 0, sizeof(grid));
-}
-
-int query()
-{
-    q++;
-    // cout << "?" << endl;
-    // for (int i = 0; i < N; i++)
-    // {
-    //     for (int j = 0; j < N; j++)
-    //         cout << grid[i][j];
-    //     cout << endl;
-    // }
-    int ans;
-    // cin >> ans;
-    ans = bfs({0, 0}, {ti, tj}) && bfs({ti, tj}, {N - 1, N - 1});
-    // cout << ans << endl;
-    if (ans == -1)
-        exit(0);
-    return ans;
-}
-
-void solve()
-{
-    q = 0;
-    int low = 0, high = N - 2;
-    while (low < high)
+    void clear()
     {
-        // cout << low << " " << high << "\n";
-        int mid = low + (high - low + 1) / 2;
-        clearGrid();
-        colorRow(0, 0);
-        colorCol(mid + 1, N - 1);
-        int ans = query();
-        if (ans == 1)
-            low = mid;
-        else
-            high = mid - 1;
-    }
-    int c = low;
-    if (c == 0)
-        c++;
-    else if (c != N - 2)
-    {
-        // check if column is same as c
-        // only possible if it is in (1,c)
-        clearGrid();
-        grid[0][0] = 1;
-        colorRow(1, 1);
-        colorCol(N - 1, N - 1);
-        int v1 = query();
-        clearGrid();
-        grid[0][0] = 1;
-        for (int j = 0; j <= c; j++)
-            grid[1][j] = 1;
-        for (int j = c; j < N; j++)
-            grid[N - 1][j] = 1;
-        colorCol(c, c);
-        int v2 = query();
-        if (v1 + v2 < 2)
-            c++;
+        for (int i = 0; i < n; i++)
+            for (int j = 0; j < n; j++)
+                mat[i][j] = '0';
     }
 
-    // column found
-    // find minimum cells to be colored in column c such that grid is valid
-    low = 0, high = N - 1;
-    while (low < high)
+    void display()
     {
-        int mid = low + (high - low) / 2;
-        clearGrid();
-        for (int j = 0; j <= c; j++)
-            grid[0][j] = 1;
-        for (int i = 0; i <= mid; i++)
-            grid[i][c] = 1;
-        for (int j = c; j < N; j++)
-            grid[mid][j] = 1;
-        for (int i = mid; i < N; i++)
-            grid[i][N - 1] = 1;
-        int ans = query();
-        if (ans == 1)
-            high = mid;
-        else
-            low = mid + 1;
+        cout << "?" << endl;
+        for (string s : mat)
+            cout << s << endl;
     }
-    int r = low;
-    if (c == N - 2)
-        r++;
 
-    if (q > 14 || (ti != r) || (tj != c))
+    void colorRow(int l, int r, char ch)
     {
-        cout << "WA" << endl;
-        cout << "q : " << q << endl;
-        cout << ti << " " << tj << endl;
-        cout << r << " " << c << endl;
+        for (int i = l; i <= r; i++)
+            for (int j = 0; j < n; j++)
+                mat[i][j] = ch;
+    }
+
+    void colorCol(int l, int r, char ch)
+    {
+        for (int i = 0; i < n; i++)
+            for (int j = l; j <= r; j++)
+                mat[i][j] = ch;
+    }
+
+    int hasPath(pii src, pii des)
+    {
+        int di[2] = {0, 1}, dj[2] = {1, 0};
+        vector<vector<bool>> vis(n, vector<bool>(n));
+        queue<pii> q;
+        if (mat[src.first][src.second] == '0')
+            return false;
+        vis[src.first][src.second] = true;
+        q.push({src.first, src.second});
+        while (!q.empty())
+        {
+            pii cur = q.front();
+            q.pop();
+            for (int dir = 0; dir < 2; dir++)
+            {
+                int i = cur.first + di[dir], j = cur.second + dj[dir];
+                if (i < n && j < n && !vis[i][j] && mat[i][j] == '1')
+                {
+                    vis[i][j] = true;
+                    q.push({i, j});
+                }
+            }
+        }
+        return vis[des.first][des.second];
+    }
+
+    int query()
+    {
+        int ans;
+        display();
+        cin >> ans;
+        if (ans == -1)
+            exit(0);
+        return ans;
+    }
+
+    bool checkRow(int row)
+    {
+        clear();
+        colorRow(0, row - 1, '1');
+        colorRow(row + 1, n - 1, '1');
+        return query();
+    }
+
+    bool checkCols(vector<int> cols)
+    {
+        clear();
+        colorRow(0, 0, '1');
+        colorRow(n - 1, n - 1, '1');
+        for (int col : cols)
+        {
+            if (col - 1 >= 0)
+            {
+                for (int i = 1; i < n - 2; i++)
+                    mat[i][col - 1] = '1';
+            }
+            if (col + 1 < n)
+            {
+                for (int i = 2; i < n - 1; i++)
+                    mat[i][col + 1] = '1';
+            }
+        }
+        return query();
+    }
+
+    int colBS(int r)
+    {
+        int low = 1, high = n - 2;
+        while (low < high)
+        {
+            int mid = low + (high - low) / 2;
+            clear();
+            colorRow(n - 1, n - 1, '1');
+            colorCol(0, mid, '1');
+            colorRow(r, r, '0');
+            if (query())
+                high = mid;
+            else
+                low = mid + 1;
+        }
+        return low;
+    }
+
+    int rowBS(int c)
+    {
+        int low = 1, high = n - 2;
+        while (low < high)
+        {
+            int mid = low + (high - low) / 2;
+            clear();
+            colorRow(0, mid, '1');
+            colorCol(c, c, '0');
+            colorCol(c + 1, n - 1, '1');
+            if (query())
+                high = mid;
+            else
+                low = mid + 1;
+        }
+        return low;
+    }
+};
+
+pii solve(int n)
+{
+    grid g = grid(n);
+    int r = 0, c = 0;
+    if (g.checkRow(1))
+    {
+        r = 1;
+        c = g.colBS(r);
+    }
+    else if (g.checkRow(n - 2))
+    {
+        r = n - 2;
+        c = g.colBS(r);
     }
     else
-        correct++;
-    // row found
-    // cout << "! " << r << " " << c << endl;
-    // int f;
-    // cin >> f;
-    // if (f == 0)
-    //     exit(0);
+    {
+        int s = 2;
+        for (int i = 0; s == 2 && i < 2; i++)
+        {
+            vector<int> cols;
+            for (int j = 0; j < n; j++)
+                if (j % 3 == i)
+                    cols.pb(j);
+            if (g.checkCols(cols))
+                s = i;
+        }
+        vector<int> cols;
+        for (int j = 0; j < n; j++)
+            if (j > 0 && j < n - 1 && j % 3 == s)
+                cols.pb(j);
+        int low = 0, high = cols.size() - 1;
+        while (low < high)
+        {
+            int mid = low + (high - low) / 2;
+            g.clear();
+            g.colorRow(n - 1, n - 1, '1');
+            g.colorCol(0, cols[mid], '1');
+            vector<int> tmp(cols.begin(), cols.begin() + mid + 1);
+            if (g.checkCols(tmp))
+                high = mid;
+            else
+                low = mid + 1;
+        }
+        c = cols[low];
+        r = g.rowBS(c);
+    }
+    return {r, c};
 }
 
 int main()
@@ -174,18 +207,17 @@ int main()
     ios_base::sync_with_stdio(false);
     cin.tie(0);
     cout.tie(0);
-    int t = 50;
-    for (int i = 1; i < N - 1; i++)
+    int t;
+    cin >> t;
+    while (t-- > 0)
     {
-        for (int j = 1; j < N - 1; j++)
-        {
-            ti = i;
-            tj = j;
-            solve();
-        }
+        int n;
+        cin >> n;
+        pii res = solve(n);
+        cout << "!" << endl;
+        cout << res.first << " " << res.second << endl;
+        int ans;
+        cin >> ans;
     }
-    cout << correct << "\n";
-    // while (t-- > 0)
-    //     solve();
     return 0;
 }
