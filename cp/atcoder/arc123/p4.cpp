95c19cca8bfcb219d0179c879d64576fdc533829
Amurto Basu <49753722+amurto@users.noreply.github.com>
2021-07-18 23:03:10 +0530

    upsolve

--- cp/atcoder/arc123/p4.cpp
+++ cp/atcoder/arc123/p4.cpp
@@ -12,22 +12,12 @@ ll calc(vector<ll> &a, ll v, int n)
     for (int i = 1; i < n; i++)
     {
         b[i] = b[i - 1];
-        c[i] = c[i - 1];
-        ll s = b[i] + c[i];
-        if (a[i] > s)
-            b[i] += (a[i] - s);
-        else if (a[i] < s)
-            c[i] -= (s - a[i]);
+        b[i] += max(0ll, a[i] - a[i - 1]);
+        c[i] = a[i] - b[i];
     }
     ll sum = 0;
     for (int i = 0; i < n; i++)
         sum += abs(b[i]) + abs(c[i]);
-    // for (int i = 0; i < n; i++)
-    //     cout << b[i] << " ";
-    // cout << "\n";
-    // for (int i = 0; i < n; i++)
-    //     cout << c[i] << " ";
-    // cout << "\n";
     return sum;
 }
 
@@ -38,22 +28,22 @@ ll solve()
     vector<ll> a(n);
     for (int i = 0; i < n; i++)
         cin >> a[i];
-    // ll low = -1e18, high = 1e18, sum = 1e18;
-    // while (high - low > 2)
-    // {
-    //     ll m1 = low + (high - low) / 3;
-    //     ll m2 = high - (high - low) / 3;
-    //     ll f1 = calc(a, m1, n);
-    //     ll f2 = calc(a, m2, n);
-    //     sum = min(sum, min(f1, f2));
-    //     if (f1 < f2)
-    //         high = m2;
-    //     else
-    //         low = m1;
-    // }
-    // for (ll i = low; i <= high; i++)
-    //     sum = min(sum, calc(a, i, n));
-    return calc(a, 0, n);
+    ll low = -1e13, high = 1e13, sum = LLONG_MAX;
+    while (high - low > 2)
+    {
+        ll m1 = low + (high - low) / 3;
+        ll m2 = high - (high - low) / 3;
+        ll f1 = calc(a, m1, n);
+        ll f2 = calc(a, m2, n);
+        sum = min(sum, min(f1, f2));
+        if (f1 < f2)
+            high = m2;
+        else
+            low = m1;
+    }
+    for (ll i = low; i <= high; i++)
+        sum = min(sum, calc(a, i, n));
+    return sum;
 }
 
 int main()
