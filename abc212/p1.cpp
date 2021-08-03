b92fe942b41bb3560b1f5386687ae355820c37fa
amurto <amurtobasu@gmail.com>
2021-08-03 15:32:07 +0530

    practice

---
+++ abc212/p1.cpp
@@ -0,0 +1,31 @@
+#ifdef amurto
+#define TERMINAL
+#include "headers/debug.cpp"
+#else
+#define d(...) 0
+#endif
+#include <bits/stdc++.h>
+using namespace std;
+
+typedef long long ll;
+#define pb push_back
+
+string solve()
+{
+    int a, b;
+    cin >> a >> b;
+    if (a + b == a)
+        return "Gold";
+    else if (a + b == b)
+        return "Silver";
+    return "Alloy";
+}
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(0);
+    cout.tie(0);
+    cout << solve() << "\n";
+    return 0;
+}
