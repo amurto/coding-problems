378f600ee521a1348f18485b0733ab1e9c518425
amurto <amurtobasu@gmail.com>
2021-08-09 11:52:44 +0530

    practice

--- cp/icpc/icpc21prelims/p4.cpp
+++ cp/icpc/icpc21prelims/p4.cpp
@@ -11,38 +11,49 @@ typedef long long ll;
 #define pb push_back
 
 const int K = 26;
-int solve()
+ll solve()
 {
     int n, q;
     cin >> n >> q;
+    ll res = 0;
     vector<string> arr(n);
-    vector<vector<int>> pdp(K, vector<int>(K));
+    vector<vector<ll>> pdp(K, vector<ll>(K));
     for (int i = 0; i < n; i++)
     {
         cin >> arr[i];
         int sz = arr[i].length();
-        vector<vector<int>> cdp(K, vector<int>(K));
-        for (int j = 0; j < sz; j++)
+        vector<vector<ll>> cdp(K, vector<ll>(K));
+        for (int j = 0; j < K; j++)
         {
-            int cur = arr[i][j] - 'a';
-            for (int k = cur; k >= 0; k--)
+            for (int k = 0; k < sz; k++)
             {
-                // cdp[k][cur]
-                if (k == cur)
-                    cdp[k][cur]++;
-                else
-                    cdp[k][cur] = max(cdp[k][cur - 1], cdp[k][cur]) + 1;
+                if (arr[i][k] - 'a' >= j)
+                {
+                    int cur = arr[i][k] - 'a';
+                    ll b = 0;
+                    for (int c = j; c <= cur; c++)
+                        b = max(b, cdp[j][c]);
+                    cdp[j][cur] = b + 1;
+                    for (int c = cur + 1; c < K; c++)
+                        cdp[j][c] = max(cdp[j][c], cdp[j][cur]);
+                }
             }
         }
         for (int j = 0; j < K; j++)
             for (int k = j; k < K; k++)
                 pdp[j][k] = max(pdp[j][k], cdp[j][k]);
     }
-    vector<vector<int>> dp(q, vector<int>(K));
-    for (int i = 0; i < q; i++)
+    int mx = min(K, q);
+    vector<vector<ll>> dp(mx + 1, vector<ll>(K));
+    for (int i = 1; i <= mx; i++)
+    {
         for (int j = 0; j < K; j++)
             for (int k = 0; k < K; k++)
-                dp[i][j] = max(dp[i][j], dp[i - 1][k] + pdp[k][j]);
+                dp[i][j] = max(dp[i][j], dp[i - 1][k] + 1ll * pdp[k][j]);
+        for (int j = 0; j < K; j++)
+            res = max(res, dp[i][j] + 1ll * (q - i) * pdp[j][j]);
+    }
+    return res;
 }
 
 int main()
