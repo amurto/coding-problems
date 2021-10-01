b918c39ae565fbf9bdf12c542692e5dc981cbb22
Amurto Basu <49753722+amurto@users.noreply.github.com>
2021-10-01 18:21:05 +0530

    upsolve

--- cp/atcoder/arc127/p2.cpp
+++ cp/atcoder/arc127/p2.cpp
@@ -15,7 +15,7 @@ void solve()
     int n, l;
     cin >> n >> l;
     vector<vector<int>> arr(3 * n, vector<int>(l));
-    arr[0][0] = 2;
+    arr[0][0] = arr[n][0] = arr[2 * n][0] = 2;
     for (int i = 1; i < n; i++)
     {
         arr[i] = arr[i - 1];
@@ -28,26 +28,16 @@ void solve()
             else
                 arr[i][j] = 0;
         }
+        arr[i + n] = arr[i];
+        arr[i + 2 * n] = arr[i];
     }
+    array<int, 3> zeroes{1, 2, 0}, ones{2, 0, 1};
     for (int i = n; i < 2 * n; i++)
-        for (int j = l - 1; j >= 0; j--)
-            arr[i][j] = 2 - arr[i - n][j];
-    for (int j = 0; j < l; j++)
-    {
-        array<int, 3> cnt{0, 0, 0};
-        for (int i = 0; i < 2 * n; i++)
-            cnt[arr[i][j]]++;
-        int cur = 2 * n;
-        for (int p = 0; p < 3; p++)
-        {
-            while (cnt[p] < n)
-            {
-                arr[cur][j] = p;
-                cnt[p]++;
-                cur++;
-            }
-        }
-    }
+        for (int j = 0; j < l; j++)
+            arr[i][j] = zeroes[arr[i][j]];
+    for (int i = 2 * n; i < 3 * n; i++)
+        for (int j = 0; j < l; j++)
+            arr[i][j] = ones[arr[i][j]];
     for (vector<int> s : arr)
     {
         for (int x : s)
