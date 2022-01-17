af8ab27357703aea8a1c5bdb36788a0c1a50c3ae
Amurto Basu <49753722+amurto@users.noreply.github.com>
2022-01-17 11:59:22 +0530

    upsolve

--- edu121/p5.cpp
+++ edu121/p5.cpp
@@ -12,16 +12,8 @@ typedef pair<int, int> pii;
 #define pb push_back
 
 const int N = 3e5 + 5;
-int col[N], pos[N], subsum[N], cnt[2 * N];
-bool vis[2 * N];
+int col[N], pos[N], subsum[N];
 vector<int> g[N];
-vector<pii> edges;
-
-int get_id(pii edge)
-{
-    int id = lower_bound(edges.begin(), edges.end(), edge) - edges.begin();
-    return id;
-}
 
 void dfs(int cur, int last, int black)
 {
@@ -32,14 +24,26 @@ void dfs(int cur, int last, int black)
         {
             dfs(e, cur, black);
             subsum[cur] += subsum[e];
-            cnt[get_id({cur, e})] = subsum[e];
+            if (col[e] == 1)
+                pos[cur] = 1;
+            else if (subsum[e] >= 2)
+                pos[cur] = max(pos[cur], pos[e]);
         }
     }
-    if (last >= 0)
+}
+
+void re_root(int cur, int last, int black)
+{
+    if (last >= 1)
     {
-        int id = get_id({cur, last});
-        cnt[id] = black - subsum[cur];
+        if (col[last] == 1)
+            pos[cur] = 1;
+        else if (black - subsum[cur] >= 2)
+            pos[cur] = max(pos[cur], pos[last]);
     }
+    for (int e : g[cur])
+        if (e != last)
+            re_root(e, cur, black);
 }
 
 void solve()
@@ -58,46 +62,9 @@ void solve()
         cin >> u >> v;
         g[u].pb(v);
         g[v].pb(u);
-        edges.pb({u, v});
-        edges.pb({v, u});
-    }
-    sort(edges.begin(), edges.end());
-    for (int i = 0; i < 2 * n; i++)
-    {
-        cnt[i] = 0;
-        vis[i] = false;
     }
     dfs(1, -1, black);
-    int sz = (int)edges.size();
-    queue<int> q;
-    for (int i = 0; i < sz; i++)
-    {
-        if (col[edges[i].second] == 1)
-        {
-            vis[i] = true;
-            pos[edges[i].first] = 1;
-            q.push(i);
-        }
-    }
-    while (!q.empty())
-    {
-        int cur = q.front();
-        q.pop();
-        int u = edges[cur].first, v = edges[cur].second;
-        for (int e : g[u])
-        {
-            if (e != v)
-            {
-                int id = get_id({e, cur});
-                if (!vis[id] && cnt[id] >= 2)
-                {
-                    vis[id] = true;
-                    pos[e] = 1;
-                    q.push(id);
-                }
-            }
-        }
-    }
+    re_root(1, -1, black);
     for (int i = 1; i <= n; i++)
         cout << pos[i] << " ";
     cout << "\n";
